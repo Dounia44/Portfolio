72a297d1ac2be6a12cf5852080924f7616e3e982
Dounia <dounia.huber@gmail.com>
2026-01-26 14:25:35 +0100

    feat: ajout du composant Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <div className="flex items-center gap-3">
+      <div className="w-12 h-12 rounded-full border-2 border-blue-500 flex items-center justify-center">
+        <div className="w-10 h-10 rounded-full border-3 border-violet-500 flex items-center justify-center">
+          <span className="font-bold text-blue-500">HD</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
