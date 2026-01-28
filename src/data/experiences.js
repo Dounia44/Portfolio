3029042ab60b55afcc069a1dbed0281f5d2bcc6f
Dounia <dounia.huber@gmail.com>
2026-01-28 14:23:16 +0100

    feat: ajout des données parcours

---
+++ src/data/experiences.js
@@ -0,0 +1,36 @@
+export const workExperiences = [
+  {
+    id: "oc-2025",
+    year: "2025 - 2026",
+    title: "Formation Intégratrice Web",
+    company: "OpenClassrooms",
+    description:
+      "Formation diplômante Bac+2(titre RNCP niveau 5). Réalisation de projets concrets en HTML, CSS, JavaScript et React, avec un focus sur l’accessibilité, la performance et les bonnes pratiques.",
+  },
+  {
+    id: "btp-2012",
+    year: "2012 - 2025",
+    title: "Électricienne / Cheffe de chantier",
+    company: "BTP",
+    description:
+      "Expérience en tant qu’électricienne et cheffe de chantier, réalisant divers projets et assurant la bonne gestion des chantiers, Développement de compétences transversales : organisation, autonomie, communication et rigueur.",
+  },
+];
+
+export const myStory = [
+  {
+    id: "text-1",
+    content:
+      " Mon parcours ne vient pas du numérique, et c’est justement ce qui fait la différence.",
+  },
+  {
+    id: "text-2",
+    content:
+      "Mon expérience passée dans le bâtiment m’a apporté rigueur, sens de l’organisation et autonomie.",
+  },
+  {
+    id: "text-3",
+    content:
+      "Aujourd’hui, je combine une approche terrain (organisation, fiabilité, sens des responsabilités) avec des compétences techniques en intégration front-end.",
+  },
+];
