82c5b953902307fb697ddfce912666854d0c785a
Dounia <dounia.huber@gmail.com>
2026-01-30 11:59:28 +0100

    feat: ajout du composant About

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import About from "./components/About";
 import Parcours from "./components/Parcours";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Projects />
+      <About />
       <Parcours />
       <Contact />
       <Footer />
---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-20 lg:py-40 px-6 bg-linear-to-r from-blue-100 to-purple-100"
+    >
+      <div className="max-w-3xl mx-auto text-center">
+        <h2>À propos de moi</h2>
+        <p className="leading-loose">
+          Mon parcours ne vient pas du numérique, et c’est justement ce qui fait
+          la différence.
+        </p>
+        <p className="leading-loose">
+          Mon expérience passée dans le bâtiment m’a apporté rigueur, sens de
+          l’organisation et autonomie. Aujourd’hui, je combine une approche
+          terrain (organisation, fiabilité, sens des responsabilités) avec des
+          compétences techniques en intégration front-end.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
