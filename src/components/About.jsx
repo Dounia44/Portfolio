5bbcb9d961428058bac07543e8997c870a8e9377
Dounia <dounia.huber@gmail.com>
2026-01-30 13:29:15 +0100

    feat: mise à jour du titre section About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
       className="py-20 lg:py-40 px-6 bg-linear-to-r from-blue-100 to-purple-100"
     >
       <div className="max-w-3xl mx-auto text-center">
-        <h2>À propos de moi</h2>
+        <h2>Et si je vous parlais un peu de moi</h2>
         <p className="leading-loose">
           Mon parcours ne vient pas du numérique, et c’est justement ce qui fait
           la différence.
