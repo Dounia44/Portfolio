b906a5a67525442f2a4a6b1a2d4a4eb7a240a1c2
Dounia <dounia.huber@gmail.com>
2026-01-31 14:16:32 +0100

    style: correction styles globaux

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
       id="about"
       className="py-20 lg:py-40 px-6 bg-linear-to-r from-blue-100 to-purple-100"
     >
-      <div className="max-w-3xl mx-auto text-center">
-        <h2>Et si je vous parlais un peu de moi</h2>
+      <h2>Et si je vous parlais un peu de moi</h2>
+      <div className="bg-white p-10 rounded-2xl shadow-lg overflow-hidden max-w-3xl mx-auto text-center border-l-4 border-purple-500">
         <p className="leading-loose">
           Mon parcours ne vient pas du numérique, et c’est justement ce qui fait
           la différence. Mon expérience passée dans le bâtiment m’a apporté
